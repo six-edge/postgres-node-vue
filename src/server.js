f99ccf8fa875524faf8cb170b33354013d2086b5
Alex <alex@sixedge.com>
2018-09-25 21:17:05 +0200

    Fixed Server

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/../dist"));
 
 var port = process.env.PORT || 5000;
 
